959e4056c383a532a0016d926ac8fd04d665f32a
ShikshaSingh9581 <153091897+ShikshaSingh9581@users.noreply.github.com>
2023-12-14 16:56:21 +0530

    Add files via upload

---
+++ main (4).cpp
@@ -0,0 +1,11 @@
+//write a program to print nth term of an arithmatic progression(A.P)
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a=3,n=4,d=2;
+    int ans=(a+(n-1)*d);
+    cout<< ans << endl;
+    return 0;
+    
+}
