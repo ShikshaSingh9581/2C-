da660de866519e289af08d98084621b5f812cc52
ShikshaSingh9581 <153091897+ShikshaSingh9581@users.noreply.github.com>
2023-12-14 16:55:15 +0530

    Add files via upload

---
+++ main (1).cpp
@@ -0,0 +1,12 @@
+//write a code to find last digit of code ,number given by user
+#include<iostream>
+using namespace std;
+int main()
+{
+   int n,ans;
+   cout<<"enter a number";
+   cin>>n;
+   ans=n%10;
+   cout<<ans<<endl;
+   return 0;
+}
